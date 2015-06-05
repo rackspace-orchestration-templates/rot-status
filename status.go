a473631cb698a532581efa2e90a9c3c41480d055
Justin Phelps <linuturk@onitato.com>
2015-06-05 15:04:17 -0500

    reordering

--- status.go
+++ status.go
@@ -2,11 +2,11 @@ package main
 
 import "net/http"
 
+func statusPage(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Status Unknown"))
+}
+
 func main() {
 	http.HandleFunc("/", statusPage)
 	http.ListenAndServe(":8080", nil)
-}
-
-func statusPage(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("Status Unknown"))
 }
