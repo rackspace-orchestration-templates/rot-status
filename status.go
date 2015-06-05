24502ccfda2a536cf21b78aeca0d541760de3564
Justin Phelps <linuturk@onitato.com>
2015-06-05 15:12:01 -0500

    starting to use the github api

--- status.go
+++ status.go
@@ -1,12 +1,21 @@
 package main
 
+import "fmt"
 import "net/http"
+import "github.com/google/go-github/github"
 
 func statusPage(w http.ResponseWriter, req *http.Request) {
+	// Connect to Github and list public repos for rot
+	client := github.NewClient(nil)
+	opt := &github.RepositoryListByOrgOptions{Type: "public"}
+	repos, _, err := client.Repositories.ListByOrg("rackspace-orchestration-templates", opt)
+	// Write out status unknown
 	w.Write([]byte("Status Unknown"))
 }
 
 func main() {
+	// Serve up the index page
 	http.HandleFunc("/", statusPage)
+	fmt.Println("Starting HTTP Server ...")
 	http.ListenAndServe(":8080", nil)
 }
