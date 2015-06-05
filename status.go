02441dc71fa639b3815eebee4bf8ff845a9a06d3
Justin Phelps <linuturk@onitato.com>
2015-06-05 14:41:54 -0500

    basic http listening

--- status.go
+++ status.go
@@ -1,7 +1,14 @@
 package main
 
 import "fmt"
+import "net/http"
 
 func main() {
-	fmt.Println("Status Unknown")
+	http.HandleFunc("/", statusPage)
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("Listening on :8080 ...")
+}
+
+func someFunc(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Status Unknown"))
 }
